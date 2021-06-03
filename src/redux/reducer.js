da1885ac27ce4fa4026d8f3c3e4d48688f04dec7
Rakib Raza <rakibraza159@gmail.com>
2021-06-03 22:15:46 +0600

    redux reducer

---
+++ src/redux/reducer.js
@@ -0,0 +1,13 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "./action"
+
+const initialStore = {
+  apartment: []
+}
+export const reducer = (state = initialStore, action) => {
+  if (action.type === ADD_TO_CART) {
+
+  }
+  if (action.type === REMOVE_FROM_CART) {
+
+  }
+}
