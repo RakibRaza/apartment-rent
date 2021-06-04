a2dad65e1d974c3b59ea71925f6bef85a7b982a2
Rakib Raza <rakibraza159@gmail.com>
2021-06-04 17:06:42 +0600

    link edit

--- src/Components/Navber/Navber.js
+++ src/Components/Navber/Navber.js
@@ -11,18 +11,10 @@ const Navber = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-              {/* <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link> */}
-              {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown> */}
             </Nav>
             <Nav>
-              <Nav.Link href="/home">Home</Nav.Link>
+              <Link to='/'>
+                <Nav.Link href="/">Home</Nav.Link></Link>
               <Nav.Link href="/about">About us</Nav.Link>
               <Link to='/login'>
                 <Nav.Link href="/blogs">Login</Nav.Link></Link>
